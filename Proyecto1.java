51e5d681068dee56c8aaeb763fa393d3372497c0
laflopet <laflopet@gmail.com>
2022-09-25 17:22:38 -0500

    git add

--- Proyecto1.java
+++ Proyecto1.java
@@ -4,7 +4,31 @@ public class Proyecto1 {
 
     public static void main(String[] args) {
 
-        
+        // enteros
 
+        byte valor = 125;
+        int valor1 = 2342342;
+        short valor2 = 12;
+        long valor3 = 312554437738L;
+
+        // decimales
+
+        float precio = 10.8f;
+        double precio2 = 87.8d;
+
+
+        // booleanos
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // cadenas
+        char letra = 'a';
+        String mensaje = "Pronto senior";
+
+        System.out.println("Valores numericos" +"\n"+ valor +"\n"+ valor1 + "\n" + valor2 + "\n"+ valor3);
+        System.out.println("Valores decimales"+ "\n"+ precio + "\n" + precio2);
+        System.out.println("Booleanos \n" + verdadero + "\n" + falso);
+        System.out.println("Cadenas de texto \n" + letra + "\n" + mensaje);
     }
 }
